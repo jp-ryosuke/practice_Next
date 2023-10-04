da9d0103a79e6d8d62af4c1277f326beb4a1a4a4
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-04 23:09:10 +0900

    getStaticPath and getStaticProps

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,15 +1,44 @@
 import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
-export default function Post() {
+/*export default function Post() {
   return <Layout>...</Layout>;
-}
+}*/
 
-import { getAllPostIds } from '../../lib/posts';
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+      </Layout>
+    );
+  }
 
+/*import { getAllPostIds } from '../../lib/posts';
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
+  };
+}*/
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
   };
 }
