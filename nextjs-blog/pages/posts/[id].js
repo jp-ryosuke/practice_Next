0311f3a5f7fb8686be0b8054c2378185599e02e2
Ryosuke Morita <morita1ryosuke@gmail.com>
2023-10-05 22:56:56 +0900

    ページのレイアウト追加

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -2,6 +2,7 @@ import Layout from '../../components/layout';
 import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 /*export default function Post() {
   return <Layout>...</Layout>;
@@ -10,15 +11,20 @@ import Date from '../../components/date';
 export default function Post({ postData }) {
   return (
     <Layout>
-      {/* Keep the existing code here */}
-
-      {/* Replace {postData.date} with this */}
-      <Date dateString={postData.date} />
-
-      {/* Keep the existing code here */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
+
 /*import { getAllPostIds } from '../../lib/posts';
 export async function getStaticPaths() {
   const paths = getAllPostIds();
